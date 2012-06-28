4da8c96463fba75b741c3e17d8b603ccd0ab18e5
Oliver Turner <oliver.turner@instinctstudios.com>
2012-06-28 14:40:55 +0100

    Now using email as user identifier

--- src/Lille/Silex/Provider/UserProvider.php
+++ src/Lille/Silex/Provider/UserProvider.php
@@ -8,31 +8,54 @@ use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Schema\Table;
+use Monolog\Logger;
 
 class UserProvider implements UserProviderInterface
 {
+    /** @var Connection */
     private $conn;
 
-    public function __construct(Connection $conn)
-    {
-        $this->conn = $conn;
+    /** @var Logger */
+    private $logger;
+
+
+    /**
+     * @param Connection $conn
+     */
+    public function __construct(Connection $conn) {
+        $this->conn   = $conn;
     }
 
-    public function loadUserByUsername($username)
-    {
-        /** @var \PDOStatement $stmt  */
-        $stmt = $this->conn->executeQuery('SELECT * FROM user WHERE username = ?', array(strtolower($username)));
+    /**
+     * Use email instead
+     *
+     * @param string $username
+     *
+     * @return UserInterface
+     * @throws UsernameNotFoundException
+     */
+    public function loadUserByUsername($username) {
+        /**
+         * @var \PDOStatement $stmt
+         */
+        $email = filter_var(strtolower($username), FILTER_SANITIZE_EMAIL);
+
+        $stmt = $this->conn->executeQuery('SELECT * FROM user WHERE email = ?', array($email));
 
         if (!$user = $stmt->fetch()) {
             throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
         }
 
-        return new User($user['username'], $user['password'], explode(',', $user['roles']), true, true, true, true);
+        return new User($user['email'], $user['password'], explode(',', $user['roles']), true, true, true, true);
     }
 
-    public function refreshUser(UserInterface $user)
-    {
+    /**
+     * @param UserInterface $user
+     *
+     * @return UserInterface
+     * @throws UnsupportedUserException
+     */
+    public function refreshUser(UserInterface $user) {
         if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
@@ -40,8 +63,12 @@ class UserProvider implements UserProviderInterface
         return $this->loadUserByUsername($user->getUsername());
     }
 
-    public function supportsClass($class)
-    {
+    /**
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class) {
         return $class === 'Symfony\Component\Security\Core\User\User';
     }
 }
